6c149fe04363f5dbe5e06856aaa8434a6f9903c2
Jackie Ho <jackieho67@gmail.com>
2016-08-19 10:44:27 -0400

    first commit

---
+++ ProductHuntSample/app/src/main/java/JackThreadsApp.java
@@ -0,0 +1,7 @@
+import android.app.Application;
+
+/**
+ * Created by JHADI on 8/19/16.
+ */
+public class JackThreadsApp extends Application {
+}
---
+++ ProductHuntSample/app/src/main/java/ho/jackie/producthuntsample/views/TodayPostActivity.java
@@ -0,0 +1,15 @@
+package ho.jackie.producthuntsample.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ho.jackie.producthuntsample.R;
+
+public class TodayPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
